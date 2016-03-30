8706a37e9c20e960381f05832ec4e659ad9dd135
Geert Fannes <geert.fannes@auro-technologies.com>
2016-03-30 09:52:47 +0200

    added auro and gubg setup

--- src/bash/personal.gfannes.sh
+++ src/bash/personal.gfannes.sh
@@ -1,3 +1,7 @@
+#Use this file in gnome terminal by setting the "Custom command" to something "bash --init-file /home/geert/gubg/bin/personal.gfannes.sh"
+
+echo ">> Setting up my environment ..."
+
 #xmodmap $HOME/.xmodmap
 
 alias ls="ls --color "
@@ -11,4 +15,43 @@ export LS_COLORS="no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:bd=40;33;01:cd
 
 export GIT_EXTERNAL_DIFF=$gubg/bin/git_diff.sh
 
-export PATH=$PATH:$HOME/.gem/ruby/2.0.0/bin
+#>> gubg
+export gubg=$HOME/gubg
+export PATH=$gubg/bin:$PATH
+#<< gubg
+
+#>> auro
+export build_publish=$HOME/pub
+function auro_notify {
+#Remove the previous $AURO_BIN from $PATH, if any
+if [ "$AURO_BIN" != "" ]
+then
+export PATH=`echo $PATH | sed "s|:$AURO_BIN||g"`
+fi
+
+export AURO_BIN=$build_publish/bin/$build_compiler
+export PATH=$PATH:$AURO_BIN
+
+echo Switched to $build_compiler, added $AURO_BIN to PATH
+}
+function release {
+export build_compiler=gcc48_x32-release
+auro_notify
+}
+function debug {
+export build_compiler=gcc48_x32-debug
+auro_notify
+}
+release
+#<< auro
+
+echo "<< ... done"
+
+cd $HOME
+
+touches_fn=gubg.sh
+if [ -f $touches_fn ]
+then
+    echo Executing local touches from $touches_fn
+    source $touches_fn
+fi
