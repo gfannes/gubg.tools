f7391e0878924d2c664e121acaf79d68b3cc24e3
Geert Fannes <geert.fannes@auro-technologies.com>
2016-03-30 10:00:42 +0200

    details

--- src/bash/personal.gfannes.sh
+++ src/bash/personal.gfannes.sh
@@ -52,6 +52,7 @@ cd $HOME
 touches_fn=$HOME/gubg.sh
 if [ -f $touches_fn ]
 then
-    echo Executing local touches from $touches_fn
+    echo ">> Executing local touches from $touches_fn ..."
     source $touches_fn
+    echo "<< ... done"
 fi
