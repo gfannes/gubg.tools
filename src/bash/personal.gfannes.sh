9384ac86b1d31f2ef8db347ae39660c434c173a3
Geert Fannes <geert.fannes@gmail.com>
2016-09-13 23:06:06 +0200

    fix

--- src/bash/personal.gfannes.sh
+++ src/bash/personal.gfannes.sh
@@ -13,8 +13,6 @@ PS1="\[\033[1;32m\]\u\[\033[1;30m\]@\[\033[1;34m\]\h \[\033[1;34m\]\W\[\033[1;30
 
 export LS_COLORS="no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jpg=01;35:*.png=01;35:*.gif=01;35:*.bmp=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.png=01;35:*.mpg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:"
 
-export GIT_EXTERNAL_DIFF=$gubg/bin/git_diff.sh
-
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 
@@ -24,6 +22,7 @@ function qcc { git commit -am "$@"; }
 export gubg=$HOME/gubg
 export PATH=$gubg/bin:$PATH
 export EDITOR=$gubg/bin/editor
+export GIT_EXTERNAL_DIFF=$gubg/bin/git_diff.sh
 #<< gubg
 
 #>> auro
