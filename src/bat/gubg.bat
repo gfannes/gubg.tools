46cc4eb17cea57a6f73d26e617486d8226dc0a94
Geert Fannes <geert.fannes@auro-technologies.com>
2017-05-24 11:17:16 +0200

    wip

--- src/bat/gubg.bat
+++ src/bat/gubg.bat
@@ -1,5 +1,7 @@
 set PATH=%PATH%;%gubg%/bin
 
+set _ac_vs_version=2017
+
 set _ac_bs=cbs
 set _ac_bs=rbs
 set _ac_arch=x64
@@ -12,15 +14,19 @@ rem Translate the arch into something VS can understand
 if "%_ac_arch%" == "x32" set _vs_arch=x86
 if "%_ac_arch%" == "x64" set _vs_arch=x64
 
-rem VS2017, cl19.1
-set _vs_path=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build
-rem VS2015, cl19.0
-set _vs_path=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC
+rem VS2017, cl 19.1
+if "%_ac_vs_version%" == "2017" set _vs_path=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build
+rem VS2015, cl 19.0
+if "%_ac_vs_version%" == "2015" set _vs_path=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC
+rem VS2013, cl 18
+if "%_ac_vs_version%" == "2013" set _vs_path=C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC
+rem VS2010, cl 16
+if "%_ac_vs_version%" == "2010" set _vs_path=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC
 
 call "%_vs_path%\vcvarsall.bat" %_vs_arch%
 
-set dir=fusion-am4car
 set dir=auro-codec-v4
+set dir=fusion-am4car
 cd %dir%
 
 cmd
