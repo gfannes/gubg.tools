6c3c3f3c544c48908ca390732089833b607acbd2
Geert Fannes <geert.fannes@gmail.com>
2018-05-08 13:13:14 +0200

    wip

--- rakefile.rb
+++ rakefile.rb
@@ -33,7 +33,7 @@ build_ok_fn = 'gubg.build.ok'
 namespace :bash do
     task :prepare do
         case os
-        when :linux, :osx
+        when :linux, :macos
             publish('src/bash', dst: 'bin', mode: 0755)
             link_unless_exists(shared_file('bin', 'dotinputrc'), File.join(ENV['HOME'], '.inputrc'))
             publish('src/ruby', dst: 'bin', mode: 0755){|fn|fn.gsub(/\.rb$/,'')}
@@ -79,7 +79,7 @@ namespace :vim do
             link_unless_exists(shared_file('vim', 'config.linux.vim'), File.join(ENV['HOME'], '.vimrc'))
             link_unless_exists(shared_file('vim', 'ideavimrc.vim'), File.join(ENV['HOME'], '.ideavimrc'))
             publish('src/vifm', dst: 'install/vifm')
-        when :osx
+        when :macos
             link_unless_exists(shared_dir('vim'), File.join(ENV['HOME'], '.vim'))
             link_unless_exists(shared_file('bin', 'dotinputrc'), File.join(ENV['HOME'], '.inputrc'))
             publish('src/vifm', dst: 'install/vifm')
@@ -110,7 +110,7 @@ namespace :vim do
                         sh "touch #{build_ok_fn}"
                     end
                 end
-            when :windows, :osx
+            when :windows, :macos
             else raise("Unknown os #{os}") end
             # git_clone('https://github.com/Frydac', 'vim-tree')
             git_clone('https://github.com/Frydac', 'vim-auro')
@@ -130,7 +130,7 @@ namespace :git do
     task :prepare do
         bash = nil
         args = case os
-               when :linux, :osx
+               when :linux, :macos
                    bash = "\#!"+`which bash`
                    '$1 $2 $3 $4 $5'
                when :windows then '%1 %2 %3 %4 %5'
@@ -150,7 +150,7 @@ namespace :git do
                 qx: 'git branch -d',
             }.each do |fn, cmd|
                 fn = case os
-                     when :linux, :osx then fn.to_s
+                     when :linux, :macos then fn.to_s
                      when :windows
                          cmd = "git diff --ignore-submodules" if fn == :qd
                          "#{fn}.bat"
@@ -158,7 +158,7 @@ namespace :git do
                 File.open(fn, "w", 0755) do |fo|
                     puts("creating #{fn}")
                     case os
-                    when :linux, :osx then fo.puts(bash)
+                    when :linux, :macos then fo.puts(bash)
                     end
                     fo.puts([cmd].join("\n")+' '+args)
                 end unless File.exist?(fn)
