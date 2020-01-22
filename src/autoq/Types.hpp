7b558967e4725e2b4e62115d9a0054c87a3b883f
Geert Fannes <geert.fannes@gmail.com>
2020-01-22 22:59:55 +0100

    Added autoq types header

---
+++ src/autoq/Types.hpp
@@ -0,0 +1,12 @@
+#ifndef HEADER_autoq_Types_hpp_ALREADY_INCLUDED
+#define HEADER_autoq_Types_hpp_ALREADY_INCLUDED
+
+#include <vector>
+
+namespace autoq { 
+
+    using Signal = std::vector<double>;
+
+} 
+
+#endif
