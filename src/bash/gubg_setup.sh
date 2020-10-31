e962951a16509b355132866ebe60c89db836a29b
Geert Fannes <geert.fannes@gmail.com>
2020-10-31 17:10:34 +0100

    Added python path

--- src/bash/gubg_setup.sh
+++ src/bash/gubg_setup.sh
@@ -7,5 +7,9 @@ export PATH=$PATH:$HOME/.gem/ruby/2.6.0/bin
 export EDITOR=$gubg/bin/editor
 export GIT_EXTERNAL_DIFF=$gubg/bin/git_diff.sh
 
+export PYTHONPATH=$PYTHONPATH:$gubg/gubg.io/src
+export PYTHONPATH=$PYTHONPATH:$gubg/gubg.ml/src
+export PYTHONPATH=$PYTHONPATH:$gubg/gubg.algo/src
+
 # Enable ** recursion
 shopt -s globstar
