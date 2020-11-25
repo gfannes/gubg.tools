aa934ce7792833157d333f9ab74d5ab0899e3cce
Geert Fannes <geert.fannes@gmail.com>
2020-11-25 13:21:46 +0100

    wip

--- src/bash/auro_setup.sh
+++ src/bash/auro_setup.sh
@@ -1,7 +1,8 @@
 export auro_publish=$HOME/pub
+export auro_ti_base=/opt/ti/ccs1011/ccs/tools/compiler
 
 function auro_notify {
-export auro_compiler=${auro_compiler_brand}-${auro_compiler_arch}-${auro_compiler_config}${auro_compiler_subconfig}${auro_compiler_cpp}${auro_compiler_thread}${auro_compiler_pic}${auro_compiler_vlc}${auro_compiler_wwise}
+export auro_compiler=${auro_compiler_brand}-${auro_compiler_arch}-${auro_compiler_config}${auro_compiler_subconfig}${auro_compiler_cpp}${auro_compiler_thread}${auro_compiler_pic}${auro_compiler_vlc}${auro_compiler_gstreamer}${auro_compiler_wwise}
 #Remove the previous $AURO_BIN from $PATH, if any
 if [ "$AURO_BIN" != "" ]
 then
@@ -13,6 +14,7 @@ export PATH=$PATH:$AURO_BIN
 
 echo auro_compiler: $auro_compiler
 echo auro_publish:  $auro_publish
+echo auro_ti_base:  $auro_ti_base
 echo auro_test:  $auro_test
 echo Added $AURO_BIN to PATH
 }
@@ -103,6 +105,15 @@ function use_novlc {
     auro_notify
 }
 
+function use_gstreamer {
+    export auro_compiler_gstreamer=-gstreamer=1
+    auro_notify
+}
+function use_nogstreamer {
+    export auro_compiler_gstreamer=
+    auro_notify
+}
+
 function use_wwise {
     export auro_compiler_wwise=
     auro_notify
@@ -126,6 +137,7 @@ use_gcc
 release
 normal
 use_pic
+use_nogstreamer
 use_nowwise
 
 function klone {
