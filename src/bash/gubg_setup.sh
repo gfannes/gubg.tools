eb9eee77aad468f4b36d19f786ac9a452890b045
Geert Fannes <geert.fannes@gmail.com>
2021-08-23 08:19:27 +0200

    Added RUBYLIB

--- src/bash/gubg_setup.sh
+++ src/bash/gubg_setup.sh
@@ -9,6 +9,8 @@ export PATH=$PATH:$HOME/.cargo/bin
 export EDITOR=$gubg/bin/editor
 export GIT_EXTERNAL_DIFF=$gubg/bin/git_diff.sh
 
+export RUBYLIB=$RUBYLIB:$gubg/ruby
+
 export PYTHONPATH=$PYTHONPATH:$gubg/gubg.io/src
 export PYTHONPATH=$PYTHONPATH:$gubg/gubg.ml/src
 export PYTHONPATH=$PYTHONPATH:$gubg/gubg.algo/src
