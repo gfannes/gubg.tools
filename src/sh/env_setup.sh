4443107f94113a140f841c264bde744ad1e6cc7e
Geert Fannes <geert.fannes@gmail.com>
2021-10-15 08:31:18 +0200

    Added use_ccache

--- src/sh/env_setup.sh
+++ src/sh/env_setup.sh
@@ -24,3 +24,12 @@ export LS_COLORS="no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:bd=40;33;01:cd
 
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+
+function use_ccache {
+    # In this folder there are wrappers for gcc and clang that need to come
+    # first in the path
+    # manjaro
+    export PATH=/usr/lib/ccache/bin/:$PATH
+    # macos installed with brew
+    export PATH=/opt/homebrew/opt/ccache/libexec:$PATH
+}
