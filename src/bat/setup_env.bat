b77572b8ec63ebe0fc926496d8aa7eb5c1d46bd1
Geert Fannes <geert.fannes@gmail.com>
2022-09-07 16:13:27 +0200

    Added local gubg touches for windows

--- src/bat/setup_env.bat
+++ src/bat/setup_env.bat
@@ -12,5 +12,12 @@ echo HOME=%HOME%
 set PATH=%PATH%;%gubg%\bin
 echo Added %gubg%\bin to PATH
 
+if exist %HOME%\gubg.bat (
+	echo Loading local gubg touches from %HOME%\gubg.bat
+	call %HOME%\gubg.bat
+) else (
+	echo No local gubg touches file %HOME%\gubg.bat found
+)
+
 echo ...done
 @echo on
