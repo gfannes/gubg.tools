c027a2505e01823f5fda2cbd2a8811254d48d5bc
Geert Fannes <geert.fannes@gmail.com>
2024-02-02 09:37:30 +0100

    Using clang from brew must be enabled explicitly

--- src/sh/brew.sh
+++ src/sh/brew.sh
@@ -1,3 +1,5 @@
-export PATH=/usr/local/opt/llvm/bin:$PATH
-export LDFLAGS=-L/usr/local/opt/llvm/lib
-export CPPFLAGS=-I/usr/local/opt/llvm/include
+if [ $gubg_use_brew_clang == 1 ]; then
+  export PATH=/usr/local/opt/llvm/bin:$PATH
+  export LDFLAGS=-L/usr/local/opt/llvm/lib
+  export CPPFLAGS=-I/usr/local/opt/llvm/include
+fi
