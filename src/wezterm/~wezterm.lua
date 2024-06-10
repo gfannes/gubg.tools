eb2b1f7f1567b08d0f9f4abed4139f9f2d43501d
Geert Fannes <geert.fannes@auro-3d.com>
2024-06-10 16:01:38 +0200

    Support for moving tabs

--- src/wezterm/~wezterm.lua
+++ src/wezterm/~wezterm.lua
@@ -21,6 +21,16 @@ config.keys = {
   { key = '8', mods = 'ALT', action = act.ActivateTab(8-1), },
   { key = '9', mods = 'ALT', action = act.ActivateTab(9-1), },
   { key = '0', mods = 'ALT', action = act.ActivateTab(10-1), },
+  { key = '1', mods = 'CTRL', action = act.MoveTab(1-1), },
+  { key = '2', mods = 'CTRL', action = act.MoveTab(2-1), },
+  { key = '3', mods = 'CTRL', action = act.MoveTab(3-1), },
+  { key = '4', mods = 'CTRL', action = act.MoveTab(4-1), },
+  { key = '5', mods = 'CTRL', action = act.MoveTab(5-1), },
+  { key = '6', mods = 'CTRL', action = act.MoveTab(6-1), },
+  { key = '7', mods = 'CTRL', action = act.MoveTab(7-1), },
+  { key = '8', mods = 'CTRL', action = act.MoveTab(8-1), },
+  { key = '9', mods = 'CTRL', action = act.MoveTab(9-1), },
+  { key = '0', mods = 'CTRL', action = act.MoveTab(10-1), },
 
   { key = 'Enter', mods = 'ALT', action = act.DisableDefaultAssignment,},
 }
