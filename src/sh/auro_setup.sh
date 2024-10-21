cc27dc99fda77fb8292fcd19869939535bee5232
Geert Fannes <geert.fannes@auro-3d.com>
2024-10-21 14:17:57 +0200

    Added use_mss

--- src/sh/auro_setup.sh
+++ src/sh/auro_setup.sh
@@ -8,7 +8,7 @@ export PATH=$PATH:/opt/local/auro/android-ndk-r21b
 export GST_PLUGIN_PATH=$auro_bin_dir
 
 function auro_notify {
-    export auro_compiler=${auro_compiler_brand}-${auro_compiler_arch}-${auro_compiler_config}${auro_compiler_subconfig}${auro_compiler_linker}${auro_compiler_cpp}${auro_compiler_thread}${auro_compiler_pic}${auro_compiler_vlc}${auro_compiler_gstreamer}${auro_compiler_wwise}${auro_compiler_wall}${auro_compiler_color}
+    export auro_compiler=${auro_compiler_brand}-${auro_compiler_arch}-${auro_compiler_config}${auro_compiler_subconfig}${auro_compiler_linker}${auro_compiler_cpp}${auro_compiler_thread}${auro_compiler_pic}${auro_compiler_vlc}${auro_compiler_gstreamer}${auro_compiler_wwise}${auro_compiler_wall}${auro_compiler_color}${auro_compiler_mss}
     echo auro_compiler: $auro_compiler
     echo auro_publish:  $auro_publish
     echo auro_ti_base:  $auro_ti_base
@@ -29,6 +29,7 @@ export auro_compiler_vlc=
 export auro_compiler_wwise=
 export auro_compiler_wall=
 export auro_compiler_color=-color
+export auro_compiler_mss=-mss_log_in_release
 export auro_test=ut
 
 function use_gcc {
@@ -153,6 +154,15 @@ function use_color {
 }
 function use_nocolor {
     export auro_compiler_color=
+    auro_notify
+}
+
+function use_mss {
+    export auro_compiler_mss=-mss_log_in_release
+    auro_notify
+}
+function use_nomss {
+    export auro_compiler_mss=
     auro_notify
 }
 
