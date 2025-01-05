2ed662eaa8a6a00361b5cd82ebc9fb23e5ffb7b4
Geert Fannes <geert.fannes@gmail.com>
2025-01-05 17:08:40 +0100

    Added n

--- src/sh/env_setup.sh
+++ src/sh/env_setup.sh
@@ -12,8 +12,6 @@ alias myumount="sudo umount "
 
 alias gsf="git submodule foreach "
 
-alias n=nvim
-
 case $gubg_shell in
   bash)
   export PS1="\[\033[1;32m\]\u\[\033[1;32m\]@\[\033[1;34m\]\h \[\033[1;34m\]\W\[\033[1;32m\]>\[\033[0m\] "
